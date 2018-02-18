88b4caafc8ea5b332ab4dd4632393989be8785be
kkkkoyo <mmkrz.sg@gmail.com>
2018-02-18 13:20:09 +0900

    init

---
+++ Human-Heart/Human-HeartTests/Human_HeartTests.swift
@@ -0,0 +1,36 @@
+//
+//  Human_HeartTests.swift
+//  Human-HeartTests
+//
+//  Created by Arai Koyo on 2018/02/18.
+//  Copyright © 2018年 Arai Koyo. All rights reserved.
+//
+
+import XCTest
+@testable import Human_Heart
+
+class Human_HeartTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
